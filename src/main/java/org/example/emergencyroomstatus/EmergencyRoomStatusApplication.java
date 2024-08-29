a90f7debd0a3d1a8359e24e93a7f093a8a585676
jitae-moon <jitae.moon@icloud.com>
2024-08-29 19:04:21 +0900

    feat #2 - 프로젝트 설정 추가
    초기 프로젝트 설정을 추가한다.
    - Spring Web
    - MySQL
    - Lombok
    - Thymeleaf
    
    추후 dependency가 더 필요할 경우 추가 예정.

---
+++ src/main/java/org/example/emergencyroomstatus/EmergencyRoomStatusApplication.java
@@ -0,0 +1,13 @@
+package org.example.emergencyroomstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyRoomStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyRoomStatusApplication.class, args);
+	}
+
+}
